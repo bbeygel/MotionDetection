da3f2954202aea450fa0fea3d3192b16e1dd0180
Yevgeny Beygel <hbeygel@gmail.com>
2017-11-10 14:06:21 +0200

    Initial Commit

---
+++ MDProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDProject
+//
+//  Created by Yevgeny Beygel on 11/10/17.
+//  Copyright © 2017 BGU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
